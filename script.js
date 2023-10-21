114a60c2638e02aea0860eff02a4bb94a588e84b
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-10-21 22:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+const n = Math.floor(Math.random()*100);
+const btn = document.getElementById("response");
+const answer = document.getElementById("respond");
+btn.addEventListener("click",()=>{
+	const input = parseFloat(document.getElementById("guess").value);
+	if (input > n) {
+		answer.innerText = `guess lower`;
+	}
+	else if(input < n){
+		answer.innerText = `guess higher`;
+	}
+	else{
+		answer.innerText = `You got it`;
+	}
+})
